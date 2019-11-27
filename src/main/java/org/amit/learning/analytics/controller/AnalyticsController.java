8d871c30a7a6885844e63c1507128778eb85435e
Amit Samanta <amitsamanta002@gmail.com>
2019-11-28 02:26:14 +0530

    Updated controller for Order Update...

--- src/main/java/org/amit/learning/analytics/controller/AnalyticsController.java
+++ src/main/java/org/amit/learning/analytics/controller/AnalyticsController.java
@@ -1,26 +1,38 @@
 package org.amit.learning.analytics.controller;
 
 
-import org.amit.learning.analytics.dao.mapper.PsnRepository;
-import org.amit.learning.analytics.dao.model.PsnInfo;
+import org.amit.learning.analytics.dao.mapper.OrderRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class AnalyticsController {
 
+    private static final Logger logger = LoggerFactory.getLogger(AnalyticsController.class);
+
     @Autowired
     private JavaMailSender mailSender;
 
     @Autowired
-    private PsnRepository psnRepository;
+    private OrderRepository orderRepository;
 
-    @RequestMapping("/analytics/process")
-    public List<PsnInfo> ProcessingData(){
-        return  psnRepository.getPsnInfoAll();
+
+    @RequestMapping("/inventory/order/updstatus")
+    private int UpdateOrderStatus(@RequestParam("orderNumber") int orderNumber,
+                                  @RequestParam("orderStatus") String orderStatus,
+                                  @RequestHeader("user") String userName){
+
+        if(orderNumber != 0 && !orderStatus.isEmpty()){
+            logger.info("Update status request came from user - "+userName);
+            return orderRepository.updateOrderStatus(orderStatus,orderNumber,userName);
+        }
+
+
+        return 000000;
     }
 }
